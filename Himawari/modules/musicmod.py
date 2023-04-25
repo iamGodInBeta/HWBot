216908a0c5c63540ba063f8e02597900d950c18d
iamGodInBeta <131788735+iamGodInBeta@users.noreply.github.com>
2023-04-25 15:25:18 +0000

    Delete Himawari directory
    Signed-off-by: iamGodInBeta <131788735+iamGodInBeta@users.noreply.github.com>

--- Himawari/modules/musicmod.py
+++
@@ -1,9 +0,0 @@
-__help__ = """
-
-/song [link] To Download Music From Various Websites Including Youtube.
-/song [query] To Download
-/lyrics [query] Get lyrics of your desired song
-
-"""
-
-__mod_name__ = "Music"
--- Himawari/modules/watcho.py
+++
@@ -1,7 +0,0 @@
-__help__ = """ 
-Get watch order (watching sequence) of any anime series
-Usage:
-/watchorder <anime name>
-"""
-    
-__mod_name__ = "Watch Order"
