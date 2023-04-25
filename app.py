32d35511cccae42c1685635a456cb383df2c74fb
iamGodInBeta <131788735+iamGodInBeta@users.noreply.github.com>
2023-04-25 16:18:54 +0000

    Update app.py
    Signed-off-by: iamGodInBeta <131788735+iamGodInBeta@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+import httpx
+from fastapi import FastAPI, Request
 
+
+TOKEN = Token
+BASE_URL = f"https://api.telegram.org/bot{TOKEN}"
+client = httpx.AsyncClient()
+
+app = FastAPI()
+@app.post("/webhook/")
+async def webhook(req: Request):
+    data = await req.json()
+    chat_id = data['message']['chat']['id']
+    text = data['message']['text']
+
+    await client.get(f"{BASE_URL}/sendMessage?chat_id={chat_id}&text={text}")
+
+    return data
